5027e4ebbe77a68e2b563bb28c47e138f3824b39
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-01-18 09:59:25 -0500

    main start stop and page draw logic completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var mainEl = document.querySelector("main");
+var footerEl = document.querySelector("footer")
 var runGame = false;
+var game
 
 var object = {
 
@@ -12,8 +14,8 @@ var object = {
         highScores: ["Here are the high scores:"]
     },
 
-    gameState: "title-card",
-    // ["title-card", "new-game", "count-down", "game-play", "results-details", "high-scores"],
+    gameState: "titleCard",
+    // ["titleCard", "newGame", "countDown", "gamePlay", "resultsAndDetails", "highScores"],
 
     gameDynamics: {
         timePerQuestion: 15,
@@ -47,7 +49,45 @@ var object = {
 
 // 
 
-var drawPage = function(state){
+
+  // creates the titleCard format with a div with an h1 and a background image
+
+ 
+//   mainEl.appendChild(titleCard);
+
+var drawPage = function(current){
+
+    mainEl.removeChild(mainEl.childNodes[0]);  
+
+    var displayCard = document.createElement("div");
+    displayCard.className = "display-card";
+    displayCard.setAttribute("id", "displayCard");
+    
+    
+    var displayCardHeader = document.createElement("h1")
+    displayCard.appendChild(displayCardHeader);
+    displayCardHeader.setAttribute("id", "cardHeader");
+    displayCardHeader.textContent = object.displayState[current][0];
+
+    var displayCardParagraph = document.createElement("p")
+    displayCard.appendChild(displayCardParagraph);
+    displayCardParagraph.setAttribute("id", "cardParagraph");
+    displayCardParagraph.textContent = object.displayState[current][1];
+
+    var displayCardFooter = document.createElement("p")
+    displayCard.appendChild(displayCardFooter);
+    displayCardFooter.setAttribute("id", "cardFooter");
+    displayCardFooter.textContent = object.displayState[current][2];
+       
+    // Draw a new state
+    mainEl.appendChild(displayCard);
+
+
+      
+    
+
+console.log(object.gameState, " is The object.gameState");
+
 
 }
 
@@ -56,38 +96,39 @@ var startStop = function (event) {
     console.log(event.target.value);
 
     var targetEl = event.target;
+    var oldState = object.gameState;
 
     if (targetEl.matches("#start-btn")) {
 
         switch(object.gameState) {
-            case "title-card":
+            case "titleCard":
             
-                object.gameState = "new-game";
+                object.gameState = "newGame";
                 console.log(object.gameState);
                 break;
-            case "new-game":
+            case "newGame":
             
-                object.gameState ="count-down";
+                object.gameState ="countDown";
                 console.log(object.gameState);
                 break;
-            case "count-down":
+            case "countDown":
             
-                object.gameState ="game-play";
+                object.gameState ="gamePlay";
                 console.log(object.gameState);
             break;
-            case "game-play":
+            case "gamePlay":
             
-                object.gameState ="game-play";
+                object.gameState ="gamePlay";
                 console.log(object.gameState);
             break;
-            case "results-details":
+            case "resultsAndDetails":
             
-                object.gameState ="new-game";
+                object.gameState ="newGame";
                 console.log(object.gameState);
             break;
-            case "high-scores":
+            case "highScores":
             
-                object.gameState ="new-game";
+                object.gameState ="newGame";
                 console.log(object.gameState);
             break;
 
@@ -97,34 +138,34 @@ var startStop = function (event) {
         }
     } else if (targetEl.matches("#stop-btn")) {
         switch(object.gameState) {
-            case "title-card":
+            case "titleCard":
             
-                object.gameState = "title-card";
+                object.gameState = "titleCard";
                 console.log(object.gameState);
                 break;
-            case "new-game":
+            case "newGame":
             
-                object.gameState ="title-card";
+                object.gameState ="titleCard";
                 console.log(object.gameState);
                 break;
-            case "count-down":
+            case "countDown":
             
-                object.gameState ="new-game";
+                object.gameState ="newGame";
                 console.log(object.gameState);
             break;
-            case "game-play":
+            case "gamePlay":
             
-                object.gameState ="results-details";
+                object.gameState ="resultsAndDetails";
                 console.log(object.gameState);
             break;
-            case "results-details":
+            case "resultsAndDetails":
             
-                object.gameState ="high-scores";
+                object.gameState ="highScores";
                 console.log(object.gameState);
             break;
-            case "high-scores":
+            case "highScores":
             
-                object.gameState ="title-card";
+                object.gameState ="titleCard";
                 console.log(object.gameState);
             break;
 
@@ -132,10 +173,14 @@ var startStop = function (event) {
 
         } 
     }
+    
+    if (oldState !== object.gameState){
+        drawPage(object.gameState)
+    } 
 }
 
-
+// drawPage(object.gameState)
 // drawPage(titleCard);
 console.log(mainEl);
 // function listening for start/stop
-mainEl.addEventListener("click", startStop);
+footerEl.addEventListener("click", startStop);
